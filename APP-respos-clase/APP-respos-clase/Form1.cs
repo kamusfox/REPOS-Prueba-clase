aa920c470431bec02c6c422876700ab7320f9a11
kamusfox <xayrtonx@gmail.com>
2016-05-18 19:33:55 +0200

    PRIMER CAMBIO REALIZADO EN LA APLICACION

---
+++ APP-respos-clase/APP-respos-clase/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APP_respos_clase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("El otro boton es azul :D");
+        }
+    }
+}
